d3b92a25e52eb20e60b631d3f6af307eac9e7d92
AnInsaneJimJam <anand47bansal@gmail.com>
2026-01-08 21:55:37 +0530

    Add basic finite field methods

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
 fn main() {
-    fn xgcd( x: u32, y: u32) ->(u32,u32,u32){
-        let (mut old_r,mut r) = (x,y);
-        let (mut old_s,mut s) = (1,0);
-        let (mut old_t,mut t) = (0,1);
-        while r != 0 {
-            let quotient = old_r/r;
-            (old_r,r) = (r,old_r - quotient*r);
-            (old_s,s) = (s,old_s - quotient*s);
-            (old_t,t) = (r,old_t - quotient*t);
-        }   
-        return (old_s,old_t,old_r ) 
+    // fn xgcd( x: u32, y: u32) ->(u32,u32,u32){
+    //     let (mut old_r,mut r) = (x,y);
+    //     let (mut old_s,mut s) = (1,0);
+    //     let (mut old_t,mut t) = (0,1);
+    //     while r != 0 {
+    //         let quotient = old_r/r;
+    //         (old_r,r) = (r,old_r - quotient*r);
+    //         (old_s,s) = (s,old_s - quotient*s);
+    //         (old_t,t) = (r,old_t - quotient*t);
+    //     }   
+    //     return (old_s,old_t,old_r ) 
+
+    // }
+
+    struct FieldElement{
+        value: u32,
+        field: u32
+    }
+
+    // Have to look that field should be same 
+
+    impl FieldElement {
+        fn new(value: u32, field: u32) -> Self{
+            FieldElement { value: value%field, field }
+        }
+        
+        fn add(&self,other: &FieldElement) -> Self{
+            let sum = (self.value+other.value)%self.field;
+            let field = self.field;
+            FieldElement{value: sum,field}
+        }
+
+        fn mul(&self,other: &FieldElement) -> Self{
+            let product = (self.value+other.value)%self.field;
+            let field = self.field;
+            FieldElement{value: product,field}
+        }
 
     }
 
